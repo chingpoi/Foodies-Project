d3500c994b9a73ae2a08a71b691a587c7e43469b
chingpoi <87354186+chingpoi@users.noreply.github.com>
2021-10-22 13:11:57 +0800

    another one

--- foodiesproject/foodies/urls.py
+++ foodiesproject/foodies/urls.py
@@ -11,17 +11,14 @@ urlpatterns = [
     
     #PROFILE VIEWS
     path('profile/', views.ProfileView.as_view(), name="profile"),
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
     #DASHBOARD VIEWS
     path('dashboard/', views.DashboardView.as_view(), name="dashboard"),
-=======
+
     path('register', views.IndexView.register, name = "register"),
->>>>>>> Stashed changes
-=======
-    path('register', views.IndexView.register, name = "register"),
->>>>>>> Stashed changes
+
+
+
     path('createUser', views.DashboardView.AddUser, name = "userAdd"),
     path('createAddress', views.DashboardView.AddAddress, name="addressAdd"),
     path('createFood', views.DashboardView.AddFood, name="foodAdd"),
